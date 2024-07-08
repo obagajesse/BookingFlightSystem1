cf6f7805a50ff424820d7d6ac92dc9dfbffff4eb
obagajesse <obagajesse@gmail.com>
2024-07-08 12:19:47 +0300

    Booking System

---
+++ src/main/java/com/obagajesse/BookingFlightSystem1/Service/AWS/AWSs3Service.java
@@ -0,0 +1,21 @@
+package com.obagajesse.BookingFlightSystem1.Service.AWS;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+@Service
+public interface AWSs3Service {
+
+    AmazonS3 getClient();
+
+    String getPreSignedUrl(String bucketName, String key);
+
+    byte[] getObject(String bucketName, String key);
+
+    boolean deleteObject(String bucketName, String key);
+
+    PutObjectResult uploadObject(String bucketName, String key, InputStream inputStream, ObjectMetadata objectMetadata);
+}
