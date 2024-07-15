73743e792f1673a62c01ba4352f11df0d1c0a245
obagajesse <obagajesse@gmail.com>
2024-07-15 20:30:21 +0300

    Booking System

--- src/main/java/com/obagajesse/BookingFlightSystem1/BookingFlightSystem1Application.java
+++ src/main/java/com/obagajesse/BookingFlightSystem1/BookingFlightSystem1Application.java
@@ -5,15 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan("com.obagajesse.BookingFlightSystem1")
-public class BookingFlightSystem1Application extends SpringBootServletInitializer {
-
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BookingFlightSystem1Application.class);
-    }
+public class BookingFlightSystem1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BookingFlightSystem1Application.class, args);
